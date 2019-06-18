25a0863f580709648acfeec5cc18be17ec70a366
sanjay chaudhary <snjy991@gmail.com>
2019-06-18 22:56:47 +0530

    Update test.groovy

--- muleDevopsConfig/test.groovy
+++ muleDevopsConfig/test.groovy
@@ -2,6 +2,6 @@ def testfun(String commit_id, String buidpath){
  def proc = "git diff-tree --no-commit-id --name-only -r ${commit_id}".execute()
   def b = new StringBuffer()
   proc.consumeProcessErrorStream(b)
-  println proc.text
+ println "output is ${proc}"
 }
 return this
