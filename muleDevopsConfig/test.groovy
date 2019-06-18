759d9666544ed0acb474db476fdac8552b025f7a
sanjay chaudhary <snjy991@gmail.com>
2019-06-18 22:26:06 +0530

    Update test.groovy

--- muleDevopsConfig/test.groovy
+++ muleDevopsConfig/test.groovy
@@ -1,10 +1,9 @@
-def testfun(String commit_id){
+def testfun(String commit_id, String buidpath){
  def proc = "git diff-tree --no-commit-id --name-only -r ${commit_id}".execute()
   def b = new StringBuffer()
   proc.consumeProcessErrorStream(b)
   println proc.text
- println "Workespace ${workspace}"
-  new File('E:/','Example.txt').withWriter('utf-8') { 
+  new File(buidpath,'Example.txt').withWriter('utf-8') { 
          writer -> writer.writeLine 'Hello World' 
 }
 return this
