ba426d715668d9b4420c6d653afd6e010eab2cfd
Sanjay <snjy991@gmail.com>
2019-06-19 15:17:12 +0530

    updated codebase

--- muleDevopsConfig/CallProjectPipeLines.groovy
+++ muleDevopsConfig/CallProjectPipeLines.groovy
@@ -1,10 +1,10 @@
 def decideProjectPipeLineCall(List<String> mylist){
   for(int i=0 ;i<mylist.size()){
     if(mylist[i]=="Example1-master"){
-      buid job =: 'MuleApplicationCICDPipeline', wait:true, propagate:true
+      buid job : 'MuleApplicationCICDPipeline', wait:true, propagate:true
     }
     if(mylist[i]=="samplesan"){
-      buid job =: 'SampleApplicationCICDPipeline', wait:true, propagate:true
+      buid job : 'SampleApplicationCICDPipeline', wait:true, propagate:true
     }
   }
 
