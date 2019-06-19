1c6333b147a1d24f3fe82ca0bce68746e9b77617
Sanjay <snjy991@gmail.com>
2019-06-19 20:54:52 +0530

    updated code

--- muleDevopsConfig/DeployCloudHub.groovy
+++ muleDevopsConfig/DeployCloudHub.groovy
@@ -1,4 +1,4 @@
-def deployToCloudHub(String JarName){
+def deployToCloudHub(List<String> JarNames){
 	println("The run type is "+ env.DEPLOYMENT_TYPE)
 	Properties properties = new Properties()
 	String branch=GIT_BRANCH
@@ -7,21 +7,22 @@ def deployToCloudHub(String JarName){
 		File propertiesFile = new File("${workspace}/muleDevopsConfig/dev_env.properties")
 		properties.load(propertiesFile.newDataInputStream())
 		println("the value of worker"+properties.worker)
-		String PATH= "/devops/out/CDScript/working/${BUILD_NUMBER}_${JOB_NAME}/"+JarName
+		String PATH= "/devops/out/CDScript/working/${BUILD_NUMBER}_${JOB_NAME}/"+JarNames.get(0)
 		println (PATH);
 		//Based on DEPLOYMENT_TYPE deciding application is already deployed or not
 		if(env.DEPLOYMENT_TYPE == "FirstRun"){
 		     println("Deploy Application First Time...")
 		     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'anypoint-studio-snjy', usernameVariable: 'Username', passwordVariable: 'Password']]) {
                    // jarName=sh (script : 'ls /devops/out/CDScript/working/${BUILD_NUMBER}_${JOB_NAME}/',returnStdout:true)
-                    println "jar name ${JarName}"
-                    sh """anypoint-cli --username ${Username} --password ${Password} runtime-mgr cloudhub-application deploy --runtime ${properties.runtime} --workers ${properties.worker} --workerSize ${properties.workerSize} --region ${properties.region} --property "username:${Username}" ${env.APPLICATIONNAME} ${PATH} """
+                    println "jar name ${JarNames.get(0)}"
+											sh """anypoint-cli --username ${Username} --password ${Password} runtime-mgr cloudhub-application deploy --runtime ${properties.runtime} --workers ${properties.worker} --workerSize ${properties.workerSize} --region ${properties.region} --property "username:${Username}" ${env.APPLICATIONNAME} ${PATH} """
+
                 }
 							}
 	 else{
 		 println("Deploy Application....")
 		 withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'anypoint-studio-snjy', usernameVariable: 'Username', passwordVariable: 'Password']]) {
-				 println "jar name ${JarName}"
+				 println "jar name ${JarNames.get(0)}"
 				 sh """anypoint-cli --username ${Username} --password ${Password} runtime-mgr cloudhub-application modify --runtime ${properties.runtime} --workers ${properties.worker} --workerSize ${properties.workerSize} --region ${properties.region} --property "username:${Username}" ${env.APPLICATIONNAME} ${PATH} """
 		 }
 	 }
