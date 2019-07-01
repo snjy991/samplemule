3401ba0fed39a2f6e911ed26f91673f458aaa8f6
Sanjay <snjy991@gmail.com>
2019-07-01 23:27:39 +0530

    updated code

--- muleDevopsConfig/CallProjectPipeLines.groovy
+++ muleDevopsConfig/CallProjectPipeLines.groovy
@@ -1,7 +1,7 @@
 def decideProjectPipeLineCall(List<String> mylist, String Commit_id,String git_branch){
   for(int i=0 ;i<mylist.size();i++){
     if(mylist[i]=="Example1-master"){
-      build job : 'MuleApplicationCICDPipeline',parameters:[[$class: 'StringParameterValue', name: 'commit_id', value:"$Commit_id"],[$class: 'StringParameterValue', name: 'GIT_BRANCH', value:"$git_branch"]], wait:true, propagate:true
+      build job : 'MuleApplicationCICDPipeline',parameters:[[$class: 'StringParameterValue', name: 'commit_id', value:"$Commit_id"]], wait:true, propagate:true
     }
     if(mylist[i]=="samplesan"){
       build job : 'SampleApplicationCICDPipeline', parameters:[[$class: 'StringParameterValue', name: 'commit_id', value:"$Commit_id"]], wait:true, propagate:true
